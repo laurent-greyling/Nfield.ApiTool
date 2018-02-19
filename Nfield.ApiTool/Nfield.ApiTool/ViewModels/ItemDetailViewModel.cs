bce0a1060f28564a33c97d48b2f840d8b3b570eb
Laurent Greyling <l.greyling@niposoftware.com>
2018-02-19 12:59:05 +0100

    Add project files.

---
+++ Nfield.ApiTool/Nfield.ApiTool/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Nfield.ApiTool.Models;
+
+namespace Nfield.ApiTool.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
